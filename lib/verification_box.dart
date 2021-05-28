35d15f79028b3154ed99472b1071d745a0cb9bf4
laomeng <781238222@qq.com>
2021-05-28 23:16:19 +0800

    add

---
+++ lib/verification_box.dart
@@ -0,0 +1,7 @@
+
+library verification_box;
+
+export 'src/verification_box.dart';
+export 'src/verification_box_item.dart';
+
+
---
+++ test/flutter_verification_box_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_verification_box/src/verification_box.dart';
+
+void main() {
+
+}
